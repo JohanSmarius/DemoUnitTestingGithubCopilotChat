885e16d761e3c3d4112fa200bd71f2f893b44ece
Johan Smarius <jsmarius@hotmail.com>
2023-07-21 16:31:36 +0200

    Updated the code

--- DemoApp/Order.cs
+++ DemoApp/Order.cs
@@ -11,7 +11,25 @@ public class Order
     }
 
     // add property to return the total price of order
-    public decimal TotalPrice => OrderLineItems.Sum(oli => oli.Price * oli.Quantity);
+    public decimal TotalPrice
+    {
+        get
+        {
+            var total = OrderLineItems.Sum(oli => oli.Price * oli.Quantity);
+
+            // Apply a 10% discount if the total is greater than 100
+            if (total > 100)
+            {
+                total *= 0.9m;
+            } // Add a 5% discount if the total is greater than 50
+            else if (total > 50)
+            {
+                total *= 0.95m;
+            }
+
+            return total;
+        }
+    }
 
 
 }
--- TestDomainCode/OrderTests.cs
+++ TestDomainCode/OrderTests.cs
@@ -17,6 +17,82 @@ public class OrderTests
     }
 
 
+    [Fact]
+    public void TotalPrice_ShouldApply5PercentDiscountIfTotalIsJustMoreThan50()
+    {
+        // Arrange
+        var order = new Order();
+        var orderlineItem1 = new OrderLineItem { ProductName = "Product 1", Quantity = 50, Price = 1 };
+        var orderlineItem2 = new OrderLineItem { ProductName = "Product 2", Quantity = 1, Price = 1 };
+        order.AddOrderLineItem(orderlineItem1);
+        order.AddOrderLineItem(orderlineItem2);
+
+        // Act
+        decimal totalPrice = order.TotalPrice;
+
+        // Assert
+        Assert.Equal(48.45m, totalPrice);
+    }
+
+    [Fact]
+    public void TotalPrice_ShouldNotApply5PercentDiscountIfTotalIsJust49()
+    {
+        // Arrange
+        var order = new Order();
+        var orderlineItem1 = new OrderLineItem { ProductName = "Product 1", Quantity = 49, Price = 1 };
+        order.AddOrderLineItem(orderlineItem1);
+
+        // Act
+        decimal totalPrice = order.TotalPrice;
+
+        // Assert
+        Assert.Equal(49.0m, totalPrice);
+    }
+
+    [Fact]
+    public void TotalPrice_ShouldApply5PercentDiscountIfTotalIsGreaterThan50()
+    {
+        // Arrange
+        var order = new Order();
+        var orderlineItem1 = new OrderLineItem { ProductName = "Product 1", Quantity = 50, Price = 1 };
+        var orderlineItem2 = new OrderLineItem { ProductName = "Product 2", Quantity = 20, Price = 1 };
+        order.AddOrderLineItem(orderlineItem1);
+        order.AddOrderLineItem(orderlineItem2);
+       
+        // Act
+        decimal totalPrice = order.TotalPrice;
+
+        // Assert
+        Assert.Equal(66.50m, totalPrice);
+    }
+
+
+    public static IEnumerable<object[]> Data =>
+        new List<object[]>
+        {
+            new object[] { 99, 94.05m },
+            new object[] { 100, 95.0m },
+            new object[] { 101, 90.9m },
+            new object[] { 200, 180.0m },
+        };
+
+    [Theory]
+    [MemberData(nameof(Data))]
+
+    public void TotalPrice_ShouldApplyPercentDiscountTakingBoundaryValuesInConsideration(int quantity, decimal totalPriceExpected)
+    {
+        // Arrange
+        var order = new Order();
+        var orderlineItem1 = new OrderLineItem { ProductName = "Product 1", Quantity = quantity, Price = 1 };
+        order.AddOrderLineItem(orderlineItem1);
+
+        // Act
+        decimal totalPrice = order.TotalPrice;
+
+        // Assert
+        Assert.Equal(totalPriceExpected, totalPrice);
+    }
+
     // Create a test to check for TotalPrice when there are no OrderLineItems
     [Fact] 
     public void TestTotalPriceNoOrderLines()
